ca3326cedd35b77ae13552fc5089f447019055cd
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2023-11-13 19:46:58 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ulimaGym',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
